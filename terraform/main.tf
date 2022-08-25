3f87ae9784c65c6c1f8ecb2933609185d2406686
Paul Thiele <paul-luca.thiele@otto.de>
2022-08-25 06:39:49 +0200

    change terraform and github actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,8 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-03f87e9ce1bec353f"
   instance_type = "t2.micro"
+  key_name = "deployer-key"
+  vpc_security_group_ids = [aws_security_group.main.id]
 
   tags = {
     Name = "Test Merver"
@@ -32,15 +34,47 @@ resource "tls_private_key" "rsa" {
   rsa_bits  = 4096
 }
 
-provisioner "file" {
-source      = "../ansible"
-destination = "/home/ec2-user"
-
-connection {
-  type        = "ssh"
-  host        = aws_instance.app_server
-  user        = "ubuntu"
-  private_key = tls_private_key.rsa.private_key_pem
-  insecure    = true
+resource "local_file" "certificate" {
+  content = tls_private_key.rsa.private_key_pem
+  filename = "/home/paul/Schule/lf10/cert.pem"
 }
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress                = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    }
+  ]
 }
