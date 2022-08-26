0fcfb88d07e01a83b3bfa5623fd2e07c65e49b87
Paul Thiele <paul-luca.thiele@otto.de>
2022-08-26 12:14:25 +0200

    change terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ resource "aws_s3_bucket" "bucket" {
 
 resource "aws_s3_bucket_acl" "bucket_acl" {
   bucket = aws_s3_bucket.bucket.id
-  acl    = "public"
+  acl    = "public-read-write"
 }
 
 resource "aws_s3_bucket_cors_configuration" "bucket-configuration" {
