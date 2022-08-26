bad36cc45d983b3a1582e75c78b4d531be4aaa72
Paul Thiele <paul-luca.thiele@otto.de>
2022-08-26 12:13:27 +0200

    change terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,32 @@ resource "tls_private_key" "rsa" {
   rsa_bits  = 4096
 }
 
+resource "aws_s3_bucket" "bucket" {
+  bucket = "fileupload-lf10-prod"
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public"
+}
+
+resource "aws_s3_bucket_cors_configuration" "bucket-configuration" {
+  bucket = aws_s3_bucket.bucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+  }
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-03f87e9ce1bec353f"
   instance_type = "t2.micro"
