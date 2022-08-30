e820b74702a02ba6e7b570d71030175990a5eaef
Paul Thiele <paul-luca.thiele@otto.de>
2022-08-30 11:44:26 +0200

    change batch-size

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,7 +100,7 @@ resource "aws_lambda_event_source_mapping" "event_source_mapping" {
   event_source_arn = aws_sqs_queue.queue.arn
   enabled          = true
   function_name    = aws_lambda_function.lambda.arn
-  batch_size       = 1
+  batch_size       = 10
 }
 
 resource "aws_sqs_queue" "queue" {
