8951be8c2741b7ee9a0af991ebabf838df663a83
Paul Thiele <paul-luca.thiele@otto.de>
2022-09-01 11:04:31 +0200

    update lambda

--- src/handler/handler.py
+++ src/handler/handler.py
@@ -1,11 +1,14 @@
 import boto3
 
 def lambda_handler(event, context):
+    for record in event['Records']:
+        date = record["body"]
+
     string = "Successful upload"
     encoded_string = string.encode("utf-8")
 
     bucket_name = "fileuploadtestlf10"
-    file_name = event["body"] + ".txt"
+    file_name = str(date) + ".txt"
     s3_path = "logs/upload_log/" + file_name
 
     s3 = boto3.resource("s3")
